5508d48352f1d508be978bc4c0a2fae82c844fca
Sebastian Schuberth <sebastian.schuberth@bosch-si.com>
2019-06-21 17:16:17 +0200

    WIP

--- reporter-web-app/build.gradle.kts
+++ reporter-web-app/build.gradle.kts
@@ -5,3 +5,14 @@ plugins {
 repositories {
     jcenter()
 }
+
+kotlin {
+    target {
+        browser()
+    }
+
+    sourceSets["main"].dependencies {
+        implementation(kotlin("stdlib-js"))
+        implementation(npm("react", "^16.8.1"))
+    }
+}
